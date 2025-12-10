834b8cef8870fd05946d0a0fbc17c36f1b53b2a9
celeroon <vladislav.d.2013@gmail.com>
2025-12-10 22:41:33 +0100

    Add ICS HMI VM

---
+++ vms/linux/ics-hmi/config.rb
@@ -0,0 +1,27 @@
+config.vm.define "ics-hmi-1" do |node|
+  node.vm.box = "generic-x64/debian11"
+  node.vm.hostname = "ics-hmi-1"
+  node.vm.provider :libvirt do |domain|
+    domain.management_network_mac = "52:54:02:54:01:94"
+    domain.cpu_mode = "host-passthrough"
+    domain.machine_type = "q35"
+    domain.cpus = 2
+    domain.memory = 4096
+  end
+  node.vm.network :private_network,
+    libvirt__tunnel_type: "udp",
+    libvirt__tunnel_local_ip: "127.200.4.2",
+    libvirt__tunnel_local_port: "12004",
+    libvirt__tunnel_ip: "127.200.4.1",
+    libvirt__tunnel_port: "12004",
+    libvirt__iface_name: "eth1",
+    auto_config: false
+  # node.vm.provision "ansible" do |ansible|
+  #   ansible.compatibility_mode  = "2.0"
+  #   ansible.inventory_path      = "./ansible/inventory.ini"
+  #   ansible.playbook_command    = "./ansible/ansible-venv/bin/ansible-playbook"
+  #   ansible.playbook            = "./ansible/playbooks/ics-hmi-setup.yml"
+  #   ansible.config_file         = "./ansible/ansible.cfg"
+  #   ansible.verbose             = "v"
+  # end
+end
